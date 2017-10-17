f281357113afe0808aabcb29e8f9460656d321ca
Alena Kabardinova <zakomoldinaav@gmail.com>
2017-10-17 18:39:24 +0300

    Minor refactoring

--- Assets/Scripts/UI/CameraController.cs
+++ Assets/Scripts/UI/CameraController.cs
@@ -3,19 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
-	private float x;
-    private float y;
-    private Vector3 rotateValue;
  
     void Update()
     {
 		if (Input.GetMouseButton(1)) 
 		{
-        	y = Input.GetAxis("Mouse X");
-        	x = Input.GetAxis("Mouse Y");
-        	rotateValue = new Vector3(x, -y, 0);
-        	transform.eulerAngles = transform.eulerAngles - rotateValue;
+        	float y = Input.GetAxis("Mouse X");
+        	float x = Input.GetAxis("Mouse Y");
+        	Vector3 rotation = new Vector3(x, -y, 0);
+        	transform.eulerAngles = transform.eulerAngles - rotation;
 		}
     }
 }
