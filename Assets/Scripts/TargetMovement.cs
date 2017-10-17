c4b6b4e30db17ce4c595c9f517fe71068bb263f3
Alena Kabardinova <zakomoldinaav@gmail.com>
2017-10-17 18:09:53 +0300

    Level implemented

---
+++ Assets/Scripts/TargetMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TargetMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+	private NavMeshAgent agent;
+
+	void Awake () {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start () {
+    	agent.destination = target.position; 
+    }
+}
---
+++ Assets/Scripts/UI/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private float x;
+    private float y;
+    private Vector3 rotateValue;
+ 
+    void Update()
+    {
+		if (Input.GetMouseButton(1)) 
+		{
+        	y = Input.GetAxis("Mouse X");
+        	x = Input.GetAxis("Mouse Y");
+        	rotateValue = new Vector3(x, -y, 0);
+        	transform.eulerAngles = transform.eulerAngles - rotateValue;
+		}
+    }
+}
