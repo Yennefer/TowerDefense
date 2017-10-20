32c64d805523590c6dba94d44656e36eb18555de
Alena Kabardinova <zakomoldinaav@gmail.com>
2017-10-20 16:41:05 +0300

    EventManager added

---
+++ Assets/Scripts/Builder.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Builder : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject[] prefabsToBuild;
+
+	private UnityAction onPrefabSelected;
+	
+	public void Awake() {
+		onPrefabSelected = BuildPrefab;
+	}
+
+	public void OnMouseDown () {
+		Debug.Log("Clicked on " + gameObject.name);
+
+		EventManager.RegisterListener("", onPrefabSelected);
+	}
+
+	private BuildPrefab() {
+
+	}
+}
