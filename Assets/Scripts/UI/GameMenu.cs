db5d50569f81823d0f89c108702bc35ff8c09d1b
Alena Kabardinova <zakomoldinaav@gmail.com>
2017-10-24 14:39:54 +0300

    Event managing system implemented

---
+++ Assets/Scripts/UI/GameMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GameMenu : MonoBehaviour {
+
+	[SerializeField]
+	private Button selectButton;
+
+	private UnityAction builderListener;
+	
+	public void Awake() {
+		selectButton.onClick.AddListener(SelectClick);
+		builderListener = SelectPrefub;
+	}
+
+	public void OnEnable() {
+		EventManager.RegisterListener(Events.selectPrefub.ToString(), builderListener);
+	}
+
+	public void OnDisable() {
+		EventManager.UnregisterListener(Events.selectPrefub.ToString(), builderListener);
+	}
+
+	public void SelectClick () {
+		selectButton.gameObject.SetActive(false);
+		EventManager.TriggerEvent(Events.buildPrefub.ToString());
+	}
+
+	private void SelectPrefub() {
+		selectButton.gameObject.SetActive(true);
+	}
+}
