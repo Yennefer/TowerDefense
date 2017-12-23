964416dbc54df8ad65b90ad7ab5f11b4f3dc262d
Victor Tarnovskiy <float34@gmail.com>
2017-12-23 22:20:52 +0300

    Revert "Camera controller"

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,77 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
-	private Terrain trn;
-	private float camSpd = 0.5f;
-	private float accFactor = 3.0f;
-	private float scaleSpdFactor = 8.0f;
-
-	private void Start()
-	{
-		trn = GameObject.FindObjectOfType<Terrain>();
-	}
-
-	private void Update()
-	{
-		if (Input.GetMouseButton(1)) 
-		{
-			float y = Input.GetAxis("Mouse X");
-			float x = Input.GetAxis("Mouse Y");
-			Vector3 rotation = new Vector3(x, -y, 0);
-			transform.eulerAngles = transform.eulerAngles - rotation;
-		}
-
-		if (Input.GetKey(KeyCode.A) && transform.position.z <= trn.terrainData.size.z/2)
-		{
-			transform.Translate(Vector3.forward * camSpd, Space.World);
-		}
-
-		if (Input.GetKey(KeyCode.D) && -transform.position.z <= trn.terrainData.size.z/2)
-		{
-			transform.Translate(Vector3.back * camSpd, Space.World);
-		}
-
-		if (Input.GetKey(KeyCode.W) && transform.position.x <= trn.terrainData.size.x/2)
-		{
-			transform.Translate(Vector3.right * camSpd, Space.World);
-		}
-
-		if (Input.GetKey(KeyCode.S) && -transform.position.x <= trn.terrainData.size.x/2)
-		{
-			transform.Translate(Vector3.left * camSpd, Space.World);
-		}
-
-		if (Input.GetKey(KeyCode.Q) && transform.position.y >= 0)
-		{
-			transform.Translate(Vector3.down * camSpd);
-		}
-
-		if (Input.GetKey(KeyCode.E) && transform.position.y <= trn.terrainData.size.x/4)
-		{
-			transform.Translate(Vector3.up * camSpd);
-		}
-
-		if (Input.GetAxis("Mouse ScrollWheel") > 0)
-		{
-			transform.Translate(Vector3.forward * camSpd * scaleSpdFactor);
-		}
-
-		if (Input.GetAxis("Mouse ScrollWheel") < 0)
-		{
-			transform.Translate(Vector3.back * camSpd * scaleSpdFactor);
-		}
-
-		if (Input.GetKeyDown(KeyCode.LeftShift))
-		{
-			camSpd *= accFactor;
-		}
-
-		if (Input.GetKeyUp(KeyCode.LeftShift))
-		{
-			camSpd /= accFactor;
-		}
-	}
+ 
+    private void Update() {
+		    if (Input.GetMouseButton(1)) {
+            float y = Input.GetAxis("Mouse X");
+        	  float x = Input.GetAxis("Mouse Y");
+        	  Vector3 rotation = new Vector3(x, -y, 0);
+        	  transform.eulerAngles = transform.eulerAngles - rotation;
+			  }
+    }
 }
